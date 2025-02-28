de1ea1ae1dfc7cc964c8b340837e6f11f470516b
Verison Vicentin <verison@hotmail.com>
2025-02-28 17:25:38 -0300

    Initial commit

---
+++ src/main/java/hello/Greeter.java
@@ -0,0 +1,7 @@
+package hello;
+
+public class Greeter {
+  public String sayHello() {
+    return "Hello Azure Blob 2.0!";
+  }
+}
---
+++ src/main/java/hello/HelloAzure.java
@@ -0,0 +1,8 @@
+package hello;
+
+public class HelloAzure {
+  public static void main(String[] args) {
+    Greeter greeter = new Greeter();
+    System.out.println(greeter.sayHello());
+  }
+}
